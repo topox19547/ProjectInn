34540c9d10a54eb2e674b8e2d075e6656394c397
topox <taxom2005@gmail.com>
2024-11-19 14:04:40 +0100

    Setup the dev environment for the client

---
+++ client/ProjectInn Client/src/Client.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1>TEST!!!!</h1>
+    </div>
+  </header>
+
+  <main>
+    <h1>TEST!!!!</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
